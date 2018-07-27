a8f088f9839423788656334696b39143c2d56deb
Durvijay <durvijay.sharma025@gmail.com>
2018-07-27 08:47:37 -0500

    food recipe setup

---
+++ src/main/java/com/altimetrik/food/FoodRecipeApplication.java
@@ -0,0 +1,12 @@
+package com.altimetrik.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecipeApplication.class, args);
+	}
+}
