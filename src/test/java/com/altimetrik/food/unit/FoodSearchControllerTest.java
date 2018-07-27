5423a0990546dd26b76a0a6798fc7a8836686454
Jair Avilés <hi@jairaviles.mx>
2018-07-27 13:24:09 -0400

    remove unused imports

--- src/test/java/com/altimetrik/food/unit/FoodSearchControllerTest.java
+++ src/test/java/com/altimetrik/food/unit/FoodSearchControllerTest.java
@@ -1,7 +1,6 @@
 package com.altimetrik.food.unit;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
